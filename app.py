f3ea12c373c524b9af60088ba8aea2c8782c5b39
Cyber-Freak999 <cyberfreak999@protonmail.com>
2025-01-31 03:58:26 +0100

    fix:Formatted JSON response

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from datetime import datetime
 import pytz
-
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -19,8 +20,11 @@ tz = pytz.timezone("Africa/Lagos")
 
 @app.get("/")
 async def main():
-    return {
+    data = {
         "email": "precious.eyoh999@gmail.com",
-        "current_datetime": datetime.now(tz).isoformat(),
-        "github_url": "https://github.com/Cyber-Freak999/HNG12-Task-0",
+        "current_datetime": datetime.now(tz).strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "github_url": "https://github.com/Cyber-Freak999/HNG12-Task-0"
     }
+    
+    formatted_response = jsonable_encoder(data)
+    return JSONResponse(content=formatted_response, media_type="application/json")
